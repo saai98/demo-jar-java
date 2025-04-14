de33ce6f93b6df956ad9b69c1369ecdbed055be0
root <root@ip-172-31-35-222.ap-south-1.compute.internal>
2025-04-14 14:05:42 +0000

    src

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class DemoApplication {
+    public static void main(String[] args) throws IOException {
+        int port = 9090;
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
+        server.createContext("/hello", new HelloHandler());
+        server.setExecutor(null);
+        System.out.println("Server started on port " + port);
+        server.start();
+    }
+
+    static class HelloHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello from demo JAR app!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
+
